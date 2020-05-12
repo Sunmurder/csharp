7334531080faa65da5981234528a20683f6e3c54
Sunmurder <malaniyadr@gmail.com>
2020-05-13 00:21:14 +0100

    Gigasecond
    Gagasecond.cs complete with System.Global

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Globalization;
 
 public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-        
+        DateTime newTime = moment.AddSeconds(Math.Pow(10,9));
+        return newTime;
     }
 }
