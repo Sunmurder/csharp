ef43a1e75421944ce8efe1cffb847b2b53f12394
Sunmurder <malaniyadr@gmail.com>
2020-05-17 13:43:55 +0100

    revised logic flow

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -8,11 +8,16 @@ public class PhoneNumber
     {
         //exercise is to clean the input (e.g. +1 (613)-995-0253 to 6139950253
         //format (NXX)-NXX-XXXX where 2<n<9 && 0<x<9
-        string phoneNumberArray = new string(phoneNumber.Where(c => char.IsDigit(c)).ToArray()).ToString();
-       if (phoneNumberArray.Length == 11 && phoneNumberArray[0] == 1 && phoneNumberArray[1] >= 2 && phoneNumberArray[3] >= 2) //checking if number is 11 digits long, checking for N and return if true, delete 1st digit and return
+
+        //(223) 456-7890 => 2234567890
+
+        string phoneNumberArray = new string(phoneNumber.Where(c => char.IsDigit(c)).ToArray()).ToString(); // (223) 456-7890 => 2234567890
+        if (phoneNumberArray.Length <=9 || phoneNumberArray.Length >= 12) // lenght 2234567890 = 10
+            throw new ArgumentException(); //falls through
+        else if (phoneNumberArray.Length == 11 && phoneNumberArray[0] == 1 && phoneNumberArray[1] >= 2 && phoneNumberArray[4] >= 2) //falls through
             return phoneNumberArray = phoneNumberArray.Remove(0, 1);
-        else if (phoneNumberArray.Length == 10 && phoneNumberArray[0] >= 2 && phoneNumberArray[3] >= 2) // checking for 10 digits long and for N. return if true
-            return phoneNumberArray;
-        throw new ArgumentException(); //all else exception, where lenght>11 or <9, N is less than 2
+        else if (phoneNumberArray.Length == 10 && phoneNumberArray[0] >= 2 && phoneNumberArray[3] >= 2) // lenght 2234567890 = 10, [0]=2, [3]<9
+            return phoneNumberArray; //above is true. Return the number
+        throw new ArgumentException(); //, N is less than 2
     }
 }
