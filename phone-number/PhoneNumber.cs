880c63a5f3b9579135557effa7cd7501c38acf11
Sunmurder <malaniyadr@gmail.com>
2020-05-18 00:21:05 +0100

    Linq skip(0) added

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -11,13 +11,13 @@ public class PhoneNumber
 
         //(223) 456-7890 => 2234567890
 
-        char [] phoneNumberArray = phoneNumber.Where(c => char.IsDigit(c)).ToArray(); // (223) 456-7890 => 2234567890
-        if (phoneNumberArray.Length <= 9 || phoneNumberArray.Length >= 12) // lenght 2234567890 = 10
-            throw new ArgumentException(); //falls through
-        else if (phoneNumberArray.Length == 11 && phoneNumberArray[0] == 1 && phoneNumberArray[1] >= 2 && phoneNumberArray[4] >= 2) //falls through
-            return new string (phoneNumberArray.Skip(0).ToString());
+        char[] phoneNumberArray = phoneNumber.Where(c => char.IsDigit(c)).ToArray(); // (223) 456-7890 => 2234567890
+                //if (phoneNumberArray.Length <= 9 || phoneNumberArray.Length >= 12) // lenght 2234567890 = 10
+               //throw new ArgumentException(); //falls through
+        if (phoneNumberArray.Length == 11 && phoneNumberArray[0] == 1 && phoneNumberArray[1] >= 2 && phoneNumberArray[4] >= 2) //falls through
+            return "Throws shit";//new string (phoneNumberArray.Skip(0).ToString());
         else if (phoneNumberArray.Length == 10 && phoneNumberArray[0] >= 2 && phoneNumberArray[3] >= 2) // lenght 2234567890 = 10, [0]=2, [3]>2
-            return phoneNumberArray.ToString(); //above is true. Return the number
+            return new string(phoneNumberArray); //above is true. Return the number
         throw new ArgumentException(); //, N is less than 2
     }
 }
