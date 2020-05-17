bfa5ac1b8280615a65c7d33ad745e2aa033f0255
Sunmurder <malaniyadr@gmail.com>
2020-05-17 21:50:17 +0100

    Linq skip/take

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -15,7 +15,7 @@ public class PhoneNumber
         if (phoneNumberArray.Length <= 9 || phoneNumberArray.Length >= 12) // lenght 2234567890 = 10
             throw new ArgumentException(); //falls through
         else if (phoneNumberArray.Length == 11 && phoneNumberArray[0] == 1 && phoneNumberArray[1] >= 2 && phoneNumberArray[4] >= 2) //falls through
-            return phoneNumberArray[1..phoneNumberArray.Length].ToString();
+            return new string (phoneNumberArray.Skip(0).ToString());
         else if (phoneNumberArray.Length == 10 && phoneNumberArray[0] >= 2 && phoneNumberArray[3] >= 2) // lenght 2234567890 = 10, [0]=2, [3]>2
             return phoneNumberArray.ToString(); //above is true. Return the number
         throw new ArgumentException(); //, N is less than 2
