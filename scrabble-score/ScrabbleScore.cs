72dc3b79f0be8488e6164a6e09cefd070d895a7b
Sunmurder <malaniyadr@gmail.com>
2020-05-22 13:13:53 +0100

    Scrabble completed
    1.Input string made to have only letters. 2.Dictionary with letter/values
    3.Loop to fill-in the dictionary
    4.Replace keys with values in the string
    5.String to char array
    6.Loop to sum, converting chars to int

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class ScrabbleScore
 {
@@ -9,17 +10,17 @@ public static class ScrabbleScore
         var onlyLetters = new String(input.Where(Char.IsLetter).ToArray()).ToLower(); //adjust the string to have letters only and turn to lower case all
         Dictionary<char, int> points = new Dictionary<char, int>(); // creating dictionary to assing points value to letters
         char[] alpha = "aeioulnrstdgbcmpfhvwykjxqz".ToCharArray(); //letters
-        int[] sequence = { 1,1,1,1,1,1,1,1,1,1,2,2,3,3,3,3,4,4,4,4,4,5,8,8,10,10 }; //correspinding values
-        for (int i = 0; i < 28; i++)  //cycle to populate the dictionary
+        int[] sequence = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 8, 8, 10, 10 }; //correspinding values
+        for (int i = 0; i < 26; i++)  //cycle to populate the dictionary
         {
             points.Add(alpha[i], sequence[i]);
         }
-        //string replaces values with digits from the dictionary and sums all
-        //return the answer
-        int answer = 23;
-        return answer;
-
-
-
+        char[] answer = points.Aggregate(onlyLetters, (result, s) => result.Replace(s.Key, (char)s.Value)).ToCharArray(); // replaces keys in string with its values turns to char array
+        int result = 0;
+        for (int i = 0; i < answer.Length; i++) //cycle to add all digits
+        {
+            result = result + Convert.ToInt32(answer[i]);
+        }
+        return result; //return result
     }
 }
