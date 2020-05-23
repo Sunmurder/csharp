2a65c3d3e97335d2b1821eb11fd97199b26ca458
Sunmurder <malaniyadr@gmail.com>
2020-05-23 12:43:01 +0100

    Collatz
    Exception added for zero value.
    
    Logic sceleton drawn

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Xunit.Sdk;
 
 public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if (number <= 0)
+            throw new ArgumentOutOfRangeException(); // check the number for being more than zero
+        else if (number) // if even delete by two, check result. i++ for every transaction
+
     }
 }
